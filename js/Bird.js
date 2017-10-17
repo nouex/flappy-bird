1c9a2c2cb5cc855895c9d4e2fc2dd318f051c540
Reece Hudson <reecehudson10@gmail.com>
2017-10-17 13:12:06 -0500

    bird.width and bird.height scale based on canvas.heigth

--- js/Bird.js
+++ js/Bird.js
@@ -10,14 +10,17 @@
 function Bird(canvas, img) {
   this.img = img
   let {width, height} = img // TODO: scaledWidth
+  let scaledWidth = canvas.height * 0.0859;// 640 / 55 // canvas height / bird width => 8.5% of canvas height
+  let birdRatio  = scaledWidth / width
+  let scaledHeight = height * birdRatio
   this.canvas = canvas
-  this.height = height
-  this.width = width
+  this.height = scaledHeight
+  this.width = scaledWidth
   this.x1 = 50
-  this.x2 = width + this.x1
+  this.x2 = this.width + this.x1
   this.y1 = canvas.height / 2 - height / 2 // FIXME: should be aboveGroundHeight
   this.y1Start = this.y1
-  this.y2 = height + this.y1
+  this.y2 = this.height + this.y1
   this.upStart = null;
 }
 
@@ -30,10 +33,6 @@ Bird.prototype.updateY = function () {
   let y = (Math.pow(x, 2) * -1570) + (540 *x) + 0
   this.y1 = this.y1Start -y
   this.y2 = this.height + this.y1
-};
-
-Bird.prototype.y1Get = function () {
-
 };
 
 Bird.prototype.isXBetween = function (l, r) {
@@ -64,7 +63,7 @@ Bird.prototype.draw = function () {
   this.updateY()
   ctx.drawImage(
     birdImg, 0, 0, birdImg.width, birdImg.height,
-    this.x1, this.y1, this.x2 - this.x1, this.y2 - this.y1
+    this.x1, this.y1, this.width, this.height
   )
   // ctx.fillStyle = "green"
   // ctx.fillRect(this.x1, this.y1, birdImg.width, birdImg.height )
